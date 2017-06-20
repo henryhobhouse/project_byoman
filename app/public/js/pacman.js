aa15f8f6fbaf8e174aaed16791dd8b6fdc2642e3
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-06-20 12:08:49 +0100

    Pacman Model

--- app/public/js/pacman.js
+++ app/public/js/pacman.js
@@ -1,5 +1,23 @@
 'use strict';
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
 function PacMan () {
   this.game = true;
+  var posX = 30;
+  var posY = 20;
+
+  this.radius = 10;
+  this.fill = 'yellow';
+
+  this.draw = function(){
+    ctx.beginPath();
+    ctx.arc(posX, posY, this.radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = this.fill;
+    ctx.fill();
+    ctx.closePath();
+  };
 }
+
+var pacman = new PacMan;
+pacman.draw();
