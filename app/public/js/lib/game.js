d30e46ec25510db07447d25a07d1e81608df32e4
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-06-22 13:05:59 +0100

    remvoes commented out lines

--- app/public/js/lib/game.js
+++ app/public/js/lib/game.js
@@ -9,13 +9,6 @@ Game.prototype = {
     }
   },
 
-  // draw: function(ctx, canvasSize) {
-  //   ctx.clearRect(0, 0, canvasSize.x, canvasSize.y);
-  //   for ( var i = 0; i < this.bodies.length; i++) {
-  //     drawImg(ctx, this.bodies[i]);
-  //   }
-  // },
-
   createPacmanObject: function(canvasSize) {
     var pacman = new PacMan(canvasSize);
     this.bodies.push(pacman);
