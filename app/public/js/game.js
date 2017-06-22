69942650e60fb8234d9167cf43b8dccc4633a3d6
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-06-22 10:01:19 +0100

    Merged Game.js

--- app/public/js/game.js
+++ app/public/js/game.js
@@ -1,7 +1,24 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+var Game = function() {
+  this.bodies = [];
+};
 
-var pacman = new PacMan;
-var food = new Food(40,150);
-pacman.draw();
-food.draw();
+Game.prototype = {
+  update: function() {
+    for (var i = 0; i < this.bodies.length; i++) {
+      this.bodies[i].update();
+    }
+  },
+
+  draw: function(ctx, canvasSize) {
+    ctx.clearRect(0, 0, canvasSize.x, canvasSize.y);
+    for ( var i = 0; i < this.bodies.length; i++) {
+      drawImg(ctx, this.bodies[i]);
+    }
+  },
+
+  createPacmanObject: function(canvasSize) {
+    var pacman = new PacMan(canvasSize);
+    this.bodies.push(pacman);
+  }
+
+};
