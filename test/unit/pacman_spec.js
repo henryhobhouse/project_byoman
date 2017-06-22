fd99b82143affd7a7a2e6c56e8b64b3c689c5e39
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-06-22 10:16:12 +0100

    Changed to master verson of Pacman Spec

--- test/unit/pacman_spec.js
+++ test/unit/pacman_spec.js
@@ -1,22 +1,29 @@
-describe("Pacman", function() {
+var PacMan = require('../../app/public/js/pacman.js')
 
+describe("Pacman", function() {
+// var pacman;
+// var DoubleCanvasSize;
+//
 //   beforeEach(function() {
-//     pacman = new PacMan
+//     DoubleCanvasSize = { x: 640, y: 540 }
+//     pacman = new PacMan(DoubleCanvasSize)
+//     console.log(pacman)
 //   });
-//
-//   it('has an X position', function(){
-//     expect(pacman.posX).not.toBeNull();
-//   });
-//
-//   it('has an Y position', function(){
-//     expect(pacman.posY).not.toBeNull();
-//   });
-//
-//   it('has a radius of at least 5', function(){
-//     expect(pacman.radius).not.toBeLessThan(5);
-//   });
-//
-//   it('is yellow', function(){
-//     expect(pacman.fill).toEqual("yellow")
-//   });
-// });
+
+  // it('has an Image', function(){
+  //   console.log(new PacMan)
+  //   expect(pacman.img).not.toBeNull();
+  // });
+
+  // it('has an Y position', function(){
+  //   expect(pacman.posY).not.toBeNull();
+  // });
+  //
+  // it('has a radius of at least 5', function(){
+  //   expect(pacman.radius).not.toBeLessThan(5);
+  // });
+  //
+  // it('is yellow', function(){
+  //   expect(pacman.fill).toEqual("yellow")
+  // });
+});
