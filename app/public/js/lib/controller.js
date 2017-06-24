de649f766792e1308631890ef1fbaa3673db946c
Henry Hobhouse <henryhobhouse@gmail.com>
2017-06-24 10:50:34 +0100

    fully refactored to include new interface

--- app/public/js/lib/controller.js
+++ app/public/js/lib/controller.js
@@ -1,22 +1,15 @@
 var Controller = function(canvasId) {
   var canvas = document.getElementById(canvasId);
   var ctx = canvas.getContext('2d');
-  var canvasSize = { x: canvas.width, y: canvas.height };
-  var game = new Game();
-  var renderer = new Renderer(ctx);
+  this.canvasSize = { x: canvas.width, y: canvas.height };
+  this.game = new Game();
+  this.renderer = new Renderer(ctx);
+  var self = this;
+  
   var tick = function() {
-    this.game.update();
-    this.renderer.draw(this.canvasSize, this.game.bodies);
+    self.game.update();
+    self.renderer.draw(self.canvasSize, self.game.bodies);
     requestAnimationFrame(tick);
   };
   tick();
-
-};
-
-Controller.prototype = {
-  // tick: function() {
-  //   this.game.update();
-  //   this.renderer.draw(this.canvasSize, this.game.bodies);
-  //   requestAnimationFrame(tick);
-  // }
 };
