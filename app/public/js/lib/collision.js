cf5aa698422ad1fb819515df4a0b77ef40e70c5b
Henry Hobhouse <henryhobhouse@gmail.com>
2017-06-25 12:09:56 +0100

    minor syntax ammendment

--- app/public/js/lib/collision.js
+++ app/public/js/lib/collision.js
@@ -18,10 +18,10 @@ Collision.prototype = {
 
 var isColliding = function(obj1, obj2) {
   return  !(
-              obj1.posX + 20 <= obj2.posX ||
-               obj1.posY + 20 <=  obj2.posY ||
-               obj1.posX >= obj2.posX ||
-               obj1.posY >= obj2.posY
+    obj1.posX + 20 <= obj2.posX ||
+    obj1.posY + 20 <=  obj2.posY ||
+    obj1.posX >= obj2.posX ||
+    obj1.posY >= obj2.posY
   );
 };
 
