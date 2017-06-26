ec53d8d547091ae673c8480c4a5be03494a7e19e
Henry Hobhouse <henryhobhouse@gmail.com>
2017-06-26 09:44:54 +0100

    motionrules refactored into own help object that ghosts can also use

--- app/public/js/lib/pacman.js
+++ app/public/js/lib/pacman.js
@@ -5,7 +5,7 @@ var PacMan = function(image, controller, gridX, gridY, tileSize){
   this.img.size = 28;
   this.xSpeed = 0;
   this.ySpeed = 0;
-  this.speed = 2;
+  this.speed = 2; // Has to be an even number
   this.tileSize = tileSize;
   this.currentX = gridX;
   this.currentY = gridY;
@@ -14,95 +14,28 @@ var PacMan = function(image, controller, gridX, gridY, tileSize){
   this.posY = this.currentY * this.tileSize - this.offset;
   this.keyboard = controller;
   this.intendedDirection = 'left';
+  this.motionrules = new MotionRules(this);
 };
 
 PacMan.prototype = {
-
   update: function() {
     if (this.keyboard.keys.up) { this.intendedDirection = 'up'; }
     else if (this.keyboard.keys.left) { this.intendedDirection = 'left'; }
     else if (this.keyboard.keys.right) { this.intendedDirection = 'right'; }
     else if (this.keyboard.keys.down) { this.intendedDirection = 'down'; }
-
     this.posX += this.xSpeed;
     this.posY += this.ySpeed;
-    this.currentGrid();
-    this.availablePath();
-    this.wallBounce();
-    this.nextMove();
-    this.escapeSide();
+    this.motionrules.currentGrid();
+    this.motionrules.availablePath();
+    this.motionrules.wallBounce();
+    this.motionrules.nextMove();
+    this.motionrules.escapeSide();
   },
-
-  currentGrid: function() {
-    this.currentX = (this.posX+this.offset)/this.tileSize | 0;
-    this.currentY = (this.posY+this.offset)/this.tileSize | 0;
-  },
-
-  escapeSide: function() {
-    if (this.posX <= -this.offset) {
-      this.posX = 575 - this.offset;
-      this.xSpeed = -this.speed;
-    } else if (this.posX >= 575 - this.offset) {
-      this.posX = -this.offset;
-      this.xSpeed = this.speed;
-    }
-  },
-
   draw: function(renderer) {
     renderer.drawPacman(this);
   },
-
   velocity: function(x, y) {
     this.xSpeed = x;
     this.ySpeed = y;
-  },
-
-  availablePath: function() {
-    levelone.path[this.currentY][this.currentX+1] === 1 ? this.right = true: this.right = false;
-    levelone.path[this.currentY][this.currentX-1] === 1 ? this.left = true: this.left = false;
-    levelone.path[this.currentY+1][this.currentX] === 1 ? this.down = true: this.down = false;
-    levelone.path[this.currentY-1][this.currentX] === 1 ? this.up = true: this.up = false;
-  },
-
-  wallBounce: function() {
-    if (this.right === false && this.xSpeed > 0 && (this.posX+this.offset)/this.tileSize === this.currentX) {
-      this.velocity(0,0);
-    } else if (this.left === false && this.xSpeed < 0 && (this.posX+this.offset)/this.tileSize === this.currentX) {
-      this.velocity(0,0);
-    } else if (this.up === false && this.ySpeed < 0 && (this.posY+this.offset)/this.tileSize === this.currentY) {
-      this.velocity(0,0);
-    } else if (this.down === false && this.ySpeed > 0 && (this.posY+this.offset)/this.tileSize === this.currentY) {
-      this.velocity(0,0);
-    }
-  },
-
-  nextMove: function() {
-    switch (this.intendedDirection) {
-    case 'left':
-      if ( this.left === true && (this.posY+this.offset)/this.tileSize === this.currentY) {
-        this.velocity(-this.speed, 0);
-        this.intendedDirection = null;
-      }
-      break;
-    case 'right':
-      if (this.right === true && (this.posY+this.offset)/this.tileSize === this.currentY) {
-        this.velocity(this.speed, 0);
-        this.intendedDirection = null;
-      }
-      break;
-    case 'up':
-      if (this.up === true && (this.posX+this.offset)/this.tileSize === this.currentX) {
-        this.velocity(0, -this.speed);
-        this.intendedDirection = null;
-      }
-      break;
-    case 'down':
-      if (this.down === true && (this.posX+this.offset)/this.tileSize === this.currentX) {
-        this.velocity(0, this.speed);
-        this.intendedDirection = null;
-      }
-      break;
-    default:
-    }
-  },
+  }
 };
